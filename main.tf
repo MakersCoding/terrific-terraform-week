9f8b99519902d8eb75d3e48f5733155ebff0c1f5
George Barker <g-barker96@hotmail.co.uk>
2024-04-03 11:40:23 +0100

    initial commit with basic main.tf file

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    backend "s3" {
+        bucket = "terrific-terraform-bucket"
+        key = "terraform.tfstate"
+        region = "eu-west-2"
+        
+    }
+
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+}
+provider "aws" {
+    region = "eu-west-2"
+}
